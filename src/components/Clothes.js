5c1886eb0693b1eb564e276a4bd9cf32c9283cc3
duuda2011@gmail.com <elissonalvessilva@MacBook-Pro-de-Elisson.local>
2022-11-27 14:56:04 -0300

    feat: criado component de clothes

---
+++ src/components/Clothes.js
@@ -0,0 +1,11 @@
+export default function Clothes({ name, description, price, img }) {
+  return (
+    <div className="containershop">
+      <div className="img-item">
+        <img src={img} alt="imagem for item"></img>
+      </div>
+      <p className="description-item">{description}</p>
+      <p className="preco">{price}</p>
+    </div>
+  )
+}
